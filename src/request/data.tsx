e199aba8788c3692924f72eb1d1bc920dca145ee
Shane Rogers <shane@repllabs.com>
2021-10-04 17:19:35 -0700

    Move all data prep functions to own file

---
+++ src/request/data.tsx
@@ -0,0 +1,208 @@
+import {
+  IAuthHeaders,
+  ICheckoutData,
+  ICheckoutProps,
+  IBillingData,
+  ICheckoutActionProps,
+  ICustomerParams,
+  ICustomer,
+  IPrepareBillingDataProps,
+  IPreparePreviewInvoiceDataProps,
+  IPreviewInvoiceData,
+  IPrepareSubscriptionUpdateDataProps,
+  ISubscriptionUpdateData,
+  IFetchConfigData,
+  IPrepareFetchConfigDataProps,
+} from '../types'
+
+export const getAuthHeaders = (apiKey: string): IAuthHeaders => ({
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${apiKey}`,
+})
+
+export const getCustomerParams = (customer: ICustomer): ICustomerParams => {
+  const result = {} as ICustomerParams
+  if (customer.id) {
+    result.customer = customer.id
+  } else if (customer.email) {
+    result.customer_email = customer.email
+  }
+
+  return result
+}
+
+export const prepareFetchConfigData = (
+  props: IPrepareFetchConfigDataProps
+): IFetchConfigData => {
+  const result = {} as IFetchConfigData
+
+  if (props.customer) {
+    result.customer = props.customer
+  } else if (props.customer_email) {
+    result.customer_email = props.customer_email
+  } else if (props.email) {
+    result.email = props.email
+  }
+  if (props.prices && props.prices.length > 0) {
+    result.prices = props.prices
+  }
+  if (props.query) {
+    result.query = props.query
+  }
+
+  return result
+}
+
+export const prepareCheckoutData = (
+  checkout: ICheckoutProps | string,
+  props: Pick<
+    ICheckoutActionProps,
+    'success_url' | 'cancel_url' | 'return_url' | 'customer' | 'metadata'
+  >
+): ICheckoutData => {
+  const currentUrl = window.location.href
+  if (typeof checkout === 'string') {
+    const result = {
+      prices: [checkout],
+      cancel_url: currentUrl,
+    } as ICheckoutData
+
+    if (props.success_url) {
+      result.success_url = props.success_url
+    }
+    if (props.cancel_url) {
+      result.cancel_url = props.cancel_url
+    }
+    const returnUrl = props.return_url || currentUrl
+    if (returnUrl) {
+      result.return_url = returnUrl
+    }
+
+    const customer = getCustomerParams(props.customer)
+    for (const key in customer) {
+      result[key] = customer[key]
+    }
+
+    return result
+  } else {
+    const result = {
+      prices: typeof checkout === 'string' ? [checkout] : checkout.prices,
+      cancel_url: currentUrl,
+    } as ICheckoutData
+
+    if (props.metadata && props.metadata.id) {
+      result.id = props.metadata.id
+    }
+
+    const successUrl = checkout.success_url || props.success_url
+    if (successUrl) {
+      result.success_url = successUrl
+    }
+
+    const cancelUrl = checkout.cancel_url || props.cancel_url
+    if (cancelUrl) {
+      result.cancel_url = cancelUrl
+    }
+
+    const returnUrl = checkout.return_url || props.return_url || currentUrl
+    if (returnUrl) {
+      result.return_url = returnUrl
+    }
+
+    const customer = getCustomerParams(checkout.customer || props.customer)
+    for (const key in customer) {
+      result[key] = customer[key]
+    }
+
+    return result
+  }
+}
+
+export const prepareBillingData = ({
+  props,
+  billingProps,
+}: IPrepareBillingDataProps): IBillingData => {
+  let customer
+  if (billingProps && billingProps.customer) {
+    customer = billingProps.customer
+  } else if (props.customer && props.customer.id) {
+    customer = props.customer.id
+  }
+  if (!customer) {
+    throw new Error(
+      'A valid customer must be provided to start a billing portal session.'
+    )
+  }
+
+  return {
+    customer,
+    return_url:
+      billingProps && billingProps.return_url
+        ? billingProps.return_url
+        : props.return_url || window.location.href,
+  }
+}
+
+export const preparePreviewInvoiceData = ({
+  props,
+  previewInvoiceProps,
+}: IPreparePreviewInvoiceDataProps): IPreviewInvoiceData => {
+  let customer
+  if (previewInvoiceProps && previewInvoiceProps.customer) {
+    customer = previewInvoiceProps.customer
+  } else if (props.customer && props.customer.id) {
+    customer = props.customer.id
+  }
+  if (!customer) {
+    throw new Error('A valid customer must be provided to preview an invoice.')
+  }
+
+  if (!previewInvoiceProps.subscription) {
+    throw new Error(
+      'A valid subscription must be provided to preview an invoice.'
+    )
+  }
+
+  return {
+    customer,
+    subscription: previewInvoiceProps.subscription,
+    items: previewInvoiceProps.items || props.values.form.checkout.items || [],
+  }
+}
+
+export const prepareSubscriptionUpdateData = ({
+  props,
+  subscriptionUpdateProps,
+}: IPrepareSubscriptionUpdateDataProps): ISubscriptionUpdateData => {
+  let customer
+  if (subscriptionUpdateProps && subscriptionUpdateProps.customer) {
+    customer = subscriptionUpdateProps.customer
+  } else if (props.customer && props.customer.id) {
+    customer = props.customer.id
+  }
+  if (!customer) {
+    throw new Error(
+      'A valid customer must be provided to update a subscription.'
+    )
+  }
+
+  if (!subscriptionUpdateProps.id) {
+    throw new Error('A valid subscription must be provided to update.')
+  }
+
+  if (!subscriptionUpdateProps.items || !subscriptionUpdateProps.items.length) {
+    throw new Error('Subscription items must be provided to update.')
+  }
+
+  const result = {
+    id: subscriptionUpdateProps.id,
+    customer,
+    items: subscriptionUpdateProps.items,
+  } as ISubscriptionUpdateData
+
+  if (subscriptionUpdateProps.proration_date) {
+    result.proration_date = subscriptionUpdateProps.proration_date
+  }
+
+  return result
+}
---
+++ test/request/data.test.ts
@@ -0,0 +1,385 @@
+import chai from 'chai'
+const { assert } = chai
+import {
+  prepareCheckoutData,
+  getAuthHeaders,
+  getCustomerParams,
+  prepareFetchConfigData,
+  prepareBillingData,
+  preparePreviewInvoiceData,
+  prepareSubscriptionUpdateData,
+} from '../../src/request/data'
+import { StripeCustomerAssociation } from '../../src/types'
+
+const STUB_FETCH_QUERY = {
+  customer: {
+    associations: ['subscriptions'] as StripeCustomerAssociation[],
+  },
+}
+
+describe('request/data', () => {
+  beforeEach(() => {
+    Object.defineProperty(window, 'location', {
+      value: {
+        href: 'http://priceblocs.com',
+      },
+    })
+  })
+
+  describe('getAuthHeaders', () => {
+    it('prepares auth headers', () => {
+      const result = getAuthHeaders('api_key')
+      const target = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer api_key`,
+      }
+      assert.deepEqual(result, target)
+    })
+  })
+
+  describe('getCustomerParams', () => {
+    it('returns customer id as customer when present', () => {
+      const result = getCustomerParams({
+        id: 'cus_123',
+      })
+      const target = {
+        customer: 'cus_123',
+      }
+
+      assert.deepEqual(result, target)
+    })
+    it('returns email as customer_email when present', () => {
+      const result = getCustomerParams({
+        email: 'customer@email.com',
+      })
+      const target = {
+        customer_email: 'customer@email.com',
+      }
+
+      assert.deepEqual(result, target)
+    })
+  })
+
+  describe('prepareFetchConfigData', () => {
+    it('returns customer with data when present when present', () => {
+      const result = prepareFetchConfigData({
+        customer: 'cus_123',
+        email: 'customer@email.com',
+        prices: ['p_A_1'],
+        query: STUB_FETCH_QUERY,
+      })
+      const target = {
+        customer: 'cus_123',
+        prices: ['p_A_1'],
+        query: STUB_FETCH_QUERY,
+      }
+
+      assert.deepEqual(result, target)
+    })
+    it('returns customer_email with data when present', () => {
+      const result = prepareFetchConfigData({
+        customer_email: 'customer@email.com',
+        email: 'customer@email.com',
+        prices: ['p_A_1'],
+        query: STUB_FETCH_QUERY,
+      })
+      const target = {
+        customer_email: 'customer@email.com',
+        prices: ['p_A_1'],
+        query: STUB_FETCH_QUERY,
+      }
+
+      assert.deepEqual(result, target)
+    })
+
+    it('returns email with data when present', () => {
+      const result = prepareFetchConfigData({
+        email: 'customer@email.com',
+        prices: ['p_A_1'],
+        query: STUB_FETCH_QUERY,
+      })
+      const target = {
+        email: 'customer@email.com',
+        prices: ['p_A_1'],
+        query: STUB_FETCH_QUERY,
+      }
+
+      assert.deepEqual(result, target)
+    })
+  })
+
+  describe('prepareCheckoutData', () => {
+    describe('single price', () => {
+      it('prepares data with fallback cancel_url', () => {
+        const checkout = 'price_123'
+        const props = {
+          api_key: 'api_key',
+          success_url: 'success_url',
+          return_url: 'return_url',
+          customer: {
+            id: 'cus_123',
+          },
+        }
+        const result = prepareCheckoutData(checkout, props)
+
+        const target = {
+          prices: ['price_123'],
+          cancel_url: 'http://priceblocs.com',
+          success_url: 'success_url',
+          return_url: 'return_url',
+          customer: 'cus_123',
+        }
+
+        assert.deepEqual(result, target)
+      })
+    })
+
+    describe('checkout config', () => {
+      it('prepares data with fallback cancel_url', () => {
+        const checkout = {
+          prices: ['price_123', 'price_456'],
+        }
+        const props = {
+          api_key: 'api_key',
+          success_url: 'success_url',
+          return_url: 'return_url',
+          customer: {
+            id: 'cus_123',
+          },
+        }
+        const result = prepareCheckoutData(checkout, props)
+
+        const target = {
+          prices: ['price_123', 'price_456'],
+          cancel_url: 'http://priceblocs.com',
+          success_url: 'success_url',
+          return_url: 'return_url',
+          customer: 'cus_123',
+        }
+
+        assert.deepEqual(result, target)
+      })
+    })
+  })
+
+  describe('prepareBillingData', () => {
+    it('billing props takes precendece over initial props', () => {
+      const result = prepareBillingData({
+        props: {
+          customer: {
+            id: 'cus_123',
+          },
+          return_url: 'props_return_url',
+        },
+        billingProps: {
+          customer: 'cus_456',
+          return_url: 'billing_props_return_url',
+        },
+      })
+      const target = {
+        customer: 'cus_456',
+        return_url: 'billing_props_return_url',
+      }
+
+      assert.deepEqual(result, target)
+    })
+    it('fallback to initial props', () => {
+      const result = prepareBillingData({
+        props: {
+          customer: {
+            id: 'cus_123',
+          },
+          return_url: 'props_return_url',
+        },
+        billingProps: {},
+      })
+      const target = {
+        customer: 'cus_123',
+        return_url: 'props_return_url',
+      }
+
+      assert.deepEqual(result, target)
+    })
+
+    it('return url falls back to current location', () => {
+      const result = prepareBillingData({
+        props: {
+          customer: {
+            id: 'cus_123',
+          },
+        },
+        billingProps: {},
+      })
+      const target = {
+        customer: 'cus_123',
+        return_url: 'http://priceblocs.com',
+      }
+
+      assert.deepEqual(result, target)
+    })
+
+    it('throws an error when customer is null', () => {
+      assert.throws(
+        () =>
+          prepareBillingData({
+            props: {},
+            billingProps: {},
+          }),
+        'A valid customer must be provided to start a billing portal session.'
+      )
+    })
+  })
+
+  describe('preparePreviewInvoiceData', () => {
+    it('returns provided subscription and items', () => {
+      const result = preparePreviewInvoiceData({
+        props: {
+          customer: {
+            id: 'cus_123',
+          },
+          values: {
+            form: {
+              checkout: {
+                items: [],
+              },
+            },
+          },
+        },
+        previewInvoiceProps: {
+          customer: 'cus_456',
+          subscription: '123',
+          items: [
+            {
+              price: {
+                id: 'p_A_1',
+              },
+            },
+          ],
+        },
+      })
+      const target = {
+        customer: 'cus_456',
+        items: [
+          {
+            price: {
+              id: 'p_A_1',
+            },
+          },
+        ],
+        subscription: '123',
+      }
+
+      assert.deepEqual(result, target)
+    })
+
+    it('returns initial props customer and checkout items', () => {
+      const result = preparePreviewInvoiceData({
+        props: {
+          customer: {
+            id: 'cus_123',
+          },
+          values: {
+            form: {
+              checkout: {
+                items: [
+                  {
+                    price: {
+                      id: 'p_A_1',
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+        previewInvoiceProps: {
+          subscription: '123',
+        },
+      })
+      const target = {
+        customer: 'cus_123',
+        items: [
+          {
+            price: {
+              id: 'p_A_1',
+            },
+          },
+        ],
+        subscription: '123',
+      }
+
+      assert.deepEqual(result, target)
+    })
+  })
+
+  describe('prepareSubscriptionUpdateData', () => {
+    it('returns subscription update params for provided values', () => {
+      const result = prepareSubscriptionUpdateData({
+        props: {
+          customer: {
+            id: 'cus_123',
+          },
+        },
+        subscriptionUpdateProps: {
+          id: 'sub_123',
+          customer: 'cus_456',
+          proration_date: 1234567,
+          items: [
+            {
+              id: 'si_123',
+              price: 'p_A_1',
+            },
+          ],
+        },
+      })
+
+      const target = {
+        id: 'sub_123',
+        customer: 'cus_456',
+        proration_date: 1234567,
+        items: [
+          {
+            id: 'si_123',
+            price: 'p_A_1',
+          },
+        ],
+      }
+
+      assert.deepEqual(result, target)
+    })
+
+    it('returns subscription update params for initial values', () => {
+      const result = prepareSubscriptionUpdateData({
+        props: {
+          customer: {
+            id: 'cus_123',
+          },
+        },
+        subscriptionUpdateProps: {
+          id: 'sub_123',
+          proration_date: 1234567,
+          items: [
+            {
+              id: 'si_123',
+              price: 'p_A_1',
+            },
+          ],
+        },
+      })
+
+      const target = {
+        id: 'sub_123',
+        customer: 'cus_123',
+        proration_date: 1234567,
+        items: [
+          {
+            id: 'si_123',
+            price: 'p_A_1',
+          },
+        ],
+      }
+
+      assert.deepEqual(result, target)
+    })
+  })
+})
