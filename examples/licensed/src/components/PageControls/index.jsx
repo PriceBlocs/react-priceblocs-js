7232066e99700cfb585c6ad414161a2c0c828b6d
Shane Rogers <shane@repllabs.com>
2021-10-18 16:21:26 -0700

    Fix import for ActionButton

--- examples/licensed/src/components/PageControls/index.jsx
+++ examples/licensed/src/components/PageControls/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ActionButton from 'components/ActionButton'
+import ActionButton from '@components/ActionButton'
 import { usePriceBlocsContext } from '@priceblocs/react-priceblocs-js'
 
 const Wrapper = ({ children }) => (
