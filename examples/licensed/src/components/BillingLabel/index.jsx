742aba9cbdef3bc53a773a9bff8625b5ff447480
Shane Rogers <shane@repllabs.com>
2021-10-18 13:17:51 -0700

    Align BillingLabel to playground

--- examples/licensed/src/components/BillingLabel/index.jsx
+++ examples/licensed/src/components/BillingLabel/index.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { INTERVAL_LABELS_MAP } from "@priceblocs/react-priceblocs-js";
-import classNames from "../../utils/classNames";
+import React from 'react'
+import { INTERVAL_LABELS_MAP } from '@priceblocs/react-priceblocs-js'
+import classNames from '@utils/classNames'
 
 const BillingLabel = ({ price }) => {
-  const priceInterval = price && price.recurring && price.recurring.interval;
+  const priceInterval = price && price.recurring && price.recurring.interval
 
   return (
     <div
-      className={classNames(`text-sm text-gray-700 py-2`, {
-        invisible: !priceInterval || !price.unit_amount
-        // 'pb-2': !hasFeatures
-      })}
+      className={classNames(
+        `text-sm text-gray-700 py-2`,
+        (!priceInterval || !price.unit_amount) && 'invisible'
+      )}
     >
-      {`Billed ${INTERVAL_LABELS_MAP[priceInterval]}` || "_"}
+      {`Billed ${INTERVAL_LABELS_MAP[priceInterval]}` || '_'}
     </div>
-  );
-};
+  )
+}
 
-export default BillingLabel;
+export default BillingLabel
