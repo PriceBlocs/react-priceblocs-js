4af49e58a3393f6620fe2a3c5e3f060f708e3aef
Shane Rogers <shane@repllabs.com>
2021-11-23 14:53:47 -0800

    Update code docs

--- src/form/index.tsx
+++ src/form/index.tsx
@@ -17,11 +17,19 @@ export const checkoutAdd = (props: SetCheckoutProps) => {
     )
 
     if (priceIndex === -1) {
+      /**
+       * Add the price to checkout items
+       * - Handle single string price id being passed
+       * - Else add full CheckoutItem data shape
+       */
       checkoutItems.push(
         typeof data === 'string'
           ? ({ price: { id: data } } as CheckoutItem)
           : (data as CheckoutItem)
       )
+      /**
+       * Update the checkout items
+       */
       set(updatedValues as Values, 'form.checkout.items', checkoutItems)
       props.setValues(updatedValues)
     } else {
@@ -41,8 +49,13 @@ export const checkoutRemove = (props: SetCheckoutProps) => {
     )
 
     if (priceIndex >= 0) {
+      /**
+       * Remove the price from checkout items
+       */
       checkoutItems.splice(priceIndex, 1)
-
+      /**
+       * Update the form checkout items
+       */
       set(updatedValues as Values, 'form.checkout.items', checkoutItems)
       props.setValues(updatedValues)
     } else {
